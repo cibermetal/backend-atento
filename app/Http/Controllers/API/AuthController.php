a3fbd303bf19a4f6c2e7a44db2f1c6e12935d616
cibermetal <david.cibermetal@gmail.com>
2021-02-17 18:37:52 -0400

    Backend core
    BEC

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller {
+    
+    public function register(Request $request){
+        $validateData = $request->validate([
+            'name' => 'required|max:55',
+            'email' => 'email|required|unique:users',
+            'password' => 'required|confirmed'
+        ]);
+        $validateData['password'] = Hash::make($request->password);       
+        $user = User::create($validateData);
+        $accessToken = $user->createToken('authToken')->accessToken;
+        return response(['user' => $user, 'accessToken' => $accessToken]);
+    }
+
+    public function login(Request $request){       
+        $loginData = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);        
+        if(!auth()->attempt($loginData)) {
+            return response(['message' => 'Existe un problema con sus credenciales, por favor revise'], 401);
+        }
+        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+        return response(['user' => auth()->user(), 'access_token' => $accessToken]);
+    }
+
+
+
+
+}
